19695025a328db02a42579040108608fe9b160a3
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-12 14:57:17 -0800

    add GET request for testing

--- App.tsx
+++ App.tsx
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Test from './components/testcomponent/test';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  interface Data {
+    id: number,
+    name: string,
+    amount: number,
+    category: string,
+    date: string,
+  }
+
+  let init: Array<Data> = [];
+
+  const [expenses, setExpenses] = useState(init);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const res = await axios.get(`http://localhost:8000/expenses/`);
+      const data = res.data;
+      setExpenses(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>hello world!</Text>
-      <Test />
+      {expenses && (
+        <>
+          {expenses.map((element, index) => {
+            return (
+              <View key={index}>
+                <Text>{element.name}</Text>
+                <Text>{element.amount}</Text>
+                <Text>{element.category}</Text>
+                <Text>{element.date}</Text>
+              </View>
+            )
+          })}
+        </>
+      )}
+      <Text>hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
