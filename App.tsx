188236aa47a05e3e42a83f7dfb3e00dcc668c975
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-12 12:34:37 -0800

    test custom components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Test from './components/testcomponent/test';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>hello world!</Text>
+      <Test />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/testcomponent/test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function test(props) {
+    return (
+        <Text>
+            hello world
+        </Text>
+    );
+}
+
+export default test;
