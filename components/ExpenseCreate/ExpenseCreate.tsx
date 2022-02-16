565388e13d36f2407910b453e3e457b279fca485
jeremiahlee94 <jhwlee94@gmail.com>
2022-02-16 09:01:08 -0800

    add POST request to expense create component

--- components/ExpenseCreate/ExpenseCreate.tsx
+++ components/ExpenseCreate/ExpenseCreate.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Text, Button, View, Modal, TextInput, Pressable } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 function ExpenseCreate({ navigation, route }) {
@@ -20,7 +21,25 @@ function ExpenseCreate({ navigation, route }) {
     const [createExpense, setCreateExpense] = useState(createInit);
 
     // POST request to add new expense to database
+    const handleSubmit = async () => {
+        try {
+            // get token from async storage
+            const token = await AsyncStorage.getItem('token');
+            const res = await axios.post(`https://salty-eyrie-01871.herokuapp.com/expenses/`, createExpense, { headers: { Authorization: `Token ${token}` } });
+            const status: number = res.status;
+            if (status === 201) {
+                console.log('hello world');
+                setCreateExpense(createInit);
+                alert('you created a new expense');
+                navigation.navigate('Profile', { name: 'Lulu' });
+            }
+        } catch (error) {
+            // error logging
+            console.log(error);
+        }
+    }
     // if successful navigate to Profile
+
     // on change handler for textinputs
     const handleChange = (event, key: string) => {
         setCreateExpense({ ...createExpense, [key]: event });
@@ -32,7 +51,7 @@ function ExpenseCreate({ navigation, route }) {
             <TextInput placeholder='Amount' onChangeText={(event) => handleChange(event, 'amount')} value={createExpense.amount} />
             <TextInput placeholder='Category' onChangeText={(event) => handleChange(event, 'category')} value={createExpense.category} />
             <TextInput placeholder='Date' onChangeText={(event) => handleChange(event, 'date')} value={createExpense.date} />
-            {/* <Pressable onPress={handleChange}><Text>submit edit</Text></Pressable> */}
+            <Pressable onPress={handleSubmit} style={{ backgroundColor: 'lemonchiffon' }}><Text>create</Text></Pressable>
         </View>
     );
 }
